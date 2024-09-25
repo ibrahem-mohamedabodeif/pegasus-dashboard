f95556dca8b5ea46589da8b099b90472427563c7
ibrahem abodeif <116855893+ibrahem-mohamedabodeif@users.noreply.github.com>
2024-09-25 06:39:23 +0300

    initial commit

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://otvplojemqvwzkfqycsm.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90dnBsb2plbXF2d3prZnF5Y3NtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYxMTIyOTQsImV4cCI6MjAzMTY4ODI5NH0.tXAkqG1Q_y2P0Tgjr-lyOffdrS5T4jyon_Pb_JVKTUU";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
